3f603454195c28e9c232aa6dea312aaabd9d8b9c
Akila Hapuarachchi <akilahapuarachchi123@gmail.com>
2023-11-29 12:02:54 +0530

    my first commit

---
+++ assets/js/pages/two-step-verification.init.js
@@ -0,0 +1,3 @@
+function moveToNext(e, t) {
+  0 < e.value.length && document.getElementById("digit" + t + "-input").focus();
+}
---
+++ assets/js/pages/user-settings.init.js
@@ -0,0 +1,2 @@
+flatpickr("#datepicker-range", { mode: "range", altInput: !0, altFormat: "Y" }),
+  new Choices("#choices-multiple-skill-input", { removeItemButton: !0 });
